475f3382a882aaedab0a28e30a9a700f8051197f
Diego Perez da Costa <diegoperezdacosta@MacBook-Pro-de-Diego.local>
2024-03-05 21:17:56 -0300

    Add: Product Seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (Product::count() === 0) {
+            Product::factory()->count(100)->create();
+        }
+    }
+}
