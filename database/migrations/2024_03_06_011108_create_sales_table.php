e005b80a5559199c43b2c30129d57ba9fac739cc
Diego Perez da Costa <diegoperezdacosta@MacBook-Pro-de-Diego.local>
2024-03-06 00:10:26 -0300

    Add: Tables sales and product_sale

---
+++ database/migrations/2024_03_06_011108_create_sales_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('amount');
+            $table->enum('status', ['concluded', 'canceled']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
